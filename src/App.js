7cba6022ef834168d5664e47a0ba11ec5f81f88a
MrAdamM1 <mradamm1@MrAdamM1s-MacBook-Pro.local>
2021-01-26 00:13:48 -0600

    create GolbalStyle for the project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,48 @@
+import React, { useState } from 'react';
+import GlobalStyle from './components/GlobalStyle';
+import styled from 'styled-components';
+
 function App() {
   return (
-    <div className='App'>
+    <AppStyle>
+      <GlobalStyle />
       <h1>Hello World</h1>
-    </div>
+    </AppStyle>
   );
 }
+
+const AppStyle = styled.div`
+  font-family: sans-serif;
+  .app-background {
+    background-color: #6d6d6d;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    min-height: 100vh;
+  }
+
+  .main-container {
+    background-color: #cf5f57;
+    width: 80%;
+    height: min-content;
+    border-radius: 5px;
+    padding: 20px;
+    box-shadow: 10px 10px 26px 0px rgba(0, 0, 0, 0.35);
+  }
+
+  .title {
+    text-align: center;
+  }
+
+  .remove-button {
+    color: white;
+    background-color: red;
+  }
+
+  .total {
+    float: right;
+    padding: 10px;
+  }
+`;
 
 export default App;
---
+++ src/components/GlobalStyle.js
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from 'styled-components';
+const GlobalStyle = createGlobalStyle`
+* {
+  font-size: 24px;
+}
+
+body {
+  margin: 0;
+  padding: 0;
+  font-family: sans-serif;
+  color: white;
+
+}
+
+`;
+export default GlobalStyle;
